51dbe5864b93d84d23755fa87f3fcd346a7d136c
szlaci83 <szlaci83@gmail.com>
2017-09-20 23:03:33 +0100

    sentiment analysis added

--- streaming.py
+++ streaming.py
@@ -3,6 +3,8 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import json, pprint
 
+import sentiment
+
 #Dict from JSON that contains the user credentials to access Twitter API
 properties = json.loads(open('properties.json').read())
 
@@ -10,7 +12,12 @@ class StdOutListener(StreamListener):
     pp = pprint.PrettyPrinter(indent=4)
 
     def on_data(self, data):
-        self.pp.pprint(data)
+        tweetJSON = json.loads(data)
+        try:
+            print((tweetJSON['text'], tweetJSON['lang'], str(tweetJSON['user']['followers_count']), tweetJSON['timestamp_ms'], sentiment.analyse(tweetJSON['text']) ))
+        except KeyError:
+            pass
+       # self.pp.pprint(tweetJSON)
         return True
 
     def on_error(self, status):
@@ -23,10 +30,9 @@ def filterStream(keywordList):
     stream = Stream(auth, StdOutListener())
 
     # This line filter Twitter Streams to capture data by the keywords:
-    stream.filter(track=keywordList)
-
+    stream.filter(track=keywordList, async=True)
 
 if __name__ == '__main__':
-    theFilter = ['EUR', 'GBP']
+    theFilter = ['bitcoin']
     print("Twitter streaming API filtering for " + str(theFilter))
     filterStream(theFilter)
