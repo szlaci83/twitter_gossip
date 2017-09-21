4767d85925384a61da51c73770f10e576772d34b
szlaci83 <szlaci83@gmail.com>
2017-09-21 17:33:26 +0100

    save as json and live avg sentiment added

--- streaming.py
+++ streaming.py
@@ -10,14 +10,37 @@ properties = json.loads(open('properties.json').read())
 
 class StdOutListener(StreamListener):
     pp = pprint.PrettyPrinter(indent=4)
+    no_of_tweets = 0
+    total_sentiment_score = 0
+
+    def save_as_JSON(self, data):
+        with open('tweets.json', 'a', encoding="utf8") as outfile:
+            outfile.write(json.dumps(data, indent=4,sort_keys=True))
+            outfile.write("\n")
+            outfile.close()
 
     def on_data(self, data):
         tweetJSON = json.loads(data)
+        lineJSON = {}
+        sentiment_score = sentiment.analyse(tweetJSON['text'])
+
         try:
-            print((tweetJSON['text'], tweetJSON['lang'], str(tweetJSON['user']['followers_count']), tweetJSON['timestamp_ms'], sentiment.analyse(tweetJSON['text']) ))
+            lineJSON['text'] = tweetJSON['text']
+            lineJSON['followers_count'] = tweetJSON['user']['followers_count']
+            lineJSON['timestamp_ms'] = tweetJSON['timestamp_ms']
+            lineJSON['text'] = tweetJSON['text']
+            lineJSON['sentiment'] = sentiment_score
         except KeyError:
             pass
-       # self.pp.pprint(tweetJSON)
+        self.pp.pprint(lineJSON)
+        self.save_as_JSON(lineJSON)
+        if sentiment_score != 0 :
+            self.no_of_tweets +=1
+            self.total_sentiment_score += sentiment_score
+        try:
+            print("AVG sentiment: " + str(self.total_sentiment_score / self.no_of_tweets))
+        except ZeroDivisionError:
+            pass
         return True
 
     def on_error(self, status):
@@ -30,7 +53,7 @@ def filterStream(keywordList):
     stream = Stream(auth, StdOutListener())
 
     # This line filter Twitter Streams to capture data by the keywords:
-    stream.filter(track=keywordList, async=True)
+    stream.filter(track=keywordList)
 
 if __name__ == '__main__':
     theFilter = ['bitcoin']
