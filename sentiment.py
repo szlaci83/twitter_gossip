64aa735a7ee036c77ed73ea0c3ed38a28006d49f
laci <szlaci83@gmail.com>
2020-02-28 14:51:19 +0000

    cleanup

--- sentiment.py
+++ sentiment.py
@@ -2,12 +2,14 @@
 #from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-def analyse(text):
-    sid = SentimentIntensityAnalyzer()
-    ss = sid.polarity_scores(text)
-    return ss['compound']
+sid = SentimentIntensityAnalyzer()
 
-hotel_rev = ["Great place to be when you are in Bangalore." ,
+def analyse(text):
+    ss = sid.polarity_scores(text)
+    return ss.get('compound', None)
+
+if __name__ == '__main__':
+    hotel_rev = ["Great place to be when you are in Bangalore." ,
              "The place was being renovated when I visited so the seating was limited.",
              "Loved the ambience, loved the food",
              "The food is delicious but not over the top.",
@@ -15,11 +17,6 @@ hotel_rev = ["Great place to be when you are in Bangalore." ,
              "The place is not easy to locate",
              "Mushroom fried rice was tasty",
              "really really the worst"]
-
-empty_string = ""
-
-if __name__ == '__main__':
     for sentence in hotel_rev:
         print(sentence)
         print(analyse(sentence))
-    #print(analyse(empty_string))
